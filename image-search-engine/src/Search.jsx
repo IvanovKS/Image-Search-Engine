9c66782c107f1639e8e6fa79ed9ecd1ce63b91aa
IvanovKS <Skitchi@mail.ru>
2024-02-17 23:56:58 +0300

    feat: add Search component

---
+++ image-search-engine/src/Search.jsx
@@ -0,0 +1,15 @@
+import './Search.css';
+
+function Search() {
+  return (
+    <>
+      <h1>Image Search Engine</h1>
+      <form action="">
+        <input type="text" placeholder="Search something here..."/>
+        <button>Search</button>
+      </form>
+    </>
+  );
+}
+
+export default Search;
--- image-search-engine/src/index.js
+++ image-search-engine/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Search from './Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Search />
   </React.StrictMode>
 );
 
